337da1e20372e5637649c5334866537b0c43ac90
Pedro <pedrojalvoperezlozao@gmail.com>
2023-11-27 11:58:09 +0100

    Banda

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -56,12 +56,10 @@ public class Principal {
     }
 
     private static void mostrarMenu() {
-        System.out.println("+----------------+");
-        System.out.println("| 1. Mostrar     |");
-        System.out.println("| 2. Añadir      |");
-        System.out.println("| 3. Eliminar    |");
-        System.out.println("| 4. Salir       |");
-        System.out.println("+----------------+");
+        System.out.println(" 1. Mostrar ");
+        System.out.println(" 2. Añadir ");
+        System.out.println("3. Eliminar");
+        System.out.println("4. Salir");
         System.out.println("Elija opción 1 a 4: ");
     }
 
